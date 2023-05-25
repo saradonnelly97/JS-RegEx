5d7d2e49f19ad1664a7583d233c1d09ad1429544
saradonnelly97 <sara.donnelly8@gmail.com>
2023-05-24 20:43:06 -0400

    regex

--- regex.js
+++ regex.js
@@ -31,7 +31,8 @@ Expected Output:
 
 */ 
  
-let phoneReg = /write your regex here/
+let phoneReg = /\d{3}-\d{3}-\d{4}/g
+let phoneArr = sampleApp.match(phoneReg)
 
 /* 
 
@@ -51,7 +52,8 @@ Expected Output:
 
 */ 
 
-let emailReg = /write your regex here/
+let emailReg = /\w+@[a-zA-Z]+.[a-zA-Z]/g
+let emailArr = sampleApp.match(emailReg)
 
 /* 
 
